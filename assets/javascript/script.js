79385b2174a4e23d1bcca446ed85a00ccf53ab53
Anthony Yonta <atischillin@gmail.com>
2019-09-16 18:54:04 -0400

    created index / folder structure / base JS, css files. also initial HTML and app code

---
+++ assets/javascript/script.js
@@ -0,0 +1,48 @@
+//https://api.giphy.com/v1/gifs/search?api_key=8LSSyXVeg1F9AE3vZP6m6U0ZEVSssOv7&q=lion king&limit=25&offset=0&rating=G&lang=en
+var buttons = ['Batman', 'Superman', 'Aquaman'];
+
+function loadButtons() {
+    var listButtons = JSON.parse(localStorage.getItem('buttons'));
+    buttons = listButtons;
+}
+
+
+function renderButtons() {
+    $('.recent-search').empty();
+    for (var i = 0; i < buttons.length; i++) {
+        var buttonName = buttons[i];
+        
+        
+
+        var button = `
+        <div class="wrap-buttons">
+            <button class="btn btn-search" data-name="${buttonName}">
+            ${buttonName}
+            </button>
+            <button class="btn btn-delete fas fa-times"" data-name"${buttonName}">
+            </button>
+        </div>
+        `;
+
+        $('.recent-search').append(button);
+
+    }
+
+    localStorage.setItem('buttons', JSON.stringify(buttons));
+}
+
+loadButtons();
+renderButtons();
+
+
+$('#submit-button').on('click', function(event) {
+    event.preventDefault();
+
+
+var value = $('#search').val();
+buttons.push(value);
+renderButtons();
+
+console.log('Value: ', value);
+
+});
