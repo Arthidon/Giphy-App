613771ebce16c91df05171f73e120daa5b1f7d31
Anthony Yonta <atischillin@gmail.com>
2019-09-16 21:37:24 -0400

    worked on scope for local storage buttons more, added if / else statment, added delete button functionality

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,10 +1,15 @@
 //https://api.giphy.com/v1/gifs/search?api_key=8LSSyXVeg1F9AE3vZP6m6U0ZEVSssOv7&q=lion king&limit=25&offset=0&rating=G&lang=en
-
+$(document).ready(function(){
 
 function loadbuttons () {
-    
+    v = localStorage.getItem('buttons');
+    if (v) {
    var listButtons = JSON.parse(localStorage.getItem('buttons') );
    buttons = listButtons;
+}
+else {
+    buttons = ['Batman', 'Superman', 'Aquaman'];
+}
 }
 
 function renderButtons() {
@@ -16,11 +21,15 @@ function renderButtons() {
 
         var button = `
         <div class="wrap-buttons">
-            <button class="btn btn-search" data-name="${buttonName}">
-            ${buttonName}
-            </button>
-            <button class="btn btn-delete fas fa-times" data-name"${buttonName}">
-            </button>
+            <button 
+                class="btn btn-search" 
+                data-name ="${buttonName}"
+            >${buttonName}</button>
+            <button 
+                class="btn btn-delete fas fa-times" 
+                data-name="${buttonName}" 
+                data-index="${i}"
+            ></button>
         </div>
         `;
 
@@ -38,17 +47,30 @@ $('#submit-button').on('click', function (event) {
 
     var value = $('#search').val();
     buttons.push(value);
+    localStorage.setItem('buttons', JSON.stringify(buttons));
     renderButtons();
 
     console.log('Value: ', value);
 
 });
 
-//$(document).on('click', '.btn-delete' , function() {
+$(document).on('click', '.btn-delete' , function() {
+    console.log('hello world');
+    buttonIndex = $(this).attr('data-index')
+    console.log('Button Index: ', buttonIndex);
+    buttons.splice(buttonIndex, 1);
+    renderButtons();
 
-//});
+});
 
 var buttons = ['Batman', 'Superman', 'Aquaman'];
-localStorage.setItem('buttons', JSON.stringify(buttons));
+
 loadbuttons ();
 renderButtons();
+
+
+
+
+
+//document.ready
+})
