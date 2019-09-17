3bd6357c4b73ebe882b9e797eb5b674b74dd0557
Anthony Yonta <atischillin@gmail.com>
2019-09-16 22:18:32 -0400

    added AJAX to serachGiphy function-tests in console log correctly

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,5 @@
 //https://api.giphy.com/v1/gifs/search?api_key=8LSSyXVeg1F9AE3vZP6m6U0ZEVSssOv7&q=lion king&limit=25&offset=0&rating=G&lang=en
-$(document).ready(function(){
+$(document).ready(function () {
 
     function loadbuttons() {
         v = localStorage.getItem('buttons');
@@ -50,21 +50,40 @@ $(document).ready(function(){
         console.log('Value: ', value);
     }
 
-function searchGiphy() {
-    event.preventDefault();
-    var value = $('#search').val();
-    addButton(value);
-    localStorage.setItem('buttons', JSON.stringify(buttons));
-}
+    function searchGiphy(event) {
+        event.preventDefault();
+        var value = $('#search').val();
+        addButton(value);
+        localStorage.setItem('buttons', JSON.stringify(buttons));
 
+
+        //AJAX
+
+        url = endpoint + '&q=' + value;
+
+        $.ajax({ url: url})
+        .then(function(data){
+            console.log('Data: ', data);
+        })
+        .catch(function(error){
+            console.log('Error: ', error);
+        });
+
+        console.log('Value: ', value);
+    }
+
+
+
+
+// variables 
 var buttons = ['Batman', 'Superman', 'Aquaman'];
-
+var API_KEY = '8LSSyXVeg1F9AE3vZP6m6U0ZEVSssOv7';
+var endpoint = "http://api.giphy.com/v1/gifs/search?&api_key=8LSSyXVeg1F9AE3vZP6m6U0ZEVSssOv7";
 loadbuttons ();
 renderButtons();
 
-
+//On click events
 $(document).on('click', '.btn-delete' , removeButton);
-
 $('#submit-button').on('click', searchGiphy);
 
 
